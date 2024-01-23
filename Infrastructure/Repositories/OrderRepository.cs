a8dbb30841a3b7260682973033897ce538178b2c
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-01-23 10:33:11 +0100

    OrderRepository added async

--- Infrastructure/Repositories/OrderRepository.cs
+++ Infrastructure/Repositories/OrderRepository.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
-    internal class OrderRepository
+    public class OrderRepository : BaseRepository<OrderEntity>
     {
+        public OrderRepository(DbContext context) : base(context)
+        {
+        }
+
+        // GETALL orders for a specific customer
+        public async Task<IEnumerable<OrderEntity>> GetOrdersByCustomerIdAsync(int customerId)
+        {
+            return await _context.Set<OrderEntity>()
+                                 .Where(o => o.CustomerId == customerId)
+                                 .ToListAsync();
+        }
+
+        // Update the status of an order
+        public async Task<bool> UpdateOrderStatusAsync(int orderId, string newStatus)
+        {
+            var order = await _context.Set<OrderEntity>().FindAsync(orderId);
+            if (order != null)
+            {
+                order.Status = newStatus;
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        // Get order details by orderID
+        public async Task<OrderEntity> GetOrderWithDetailsAsync(int orderId)
+        {
+            return await _context.Set<OrderEntity>()
+                                 .Include(o => o.OrderDetails)
+                                 .FirstOrDefaultAsync(o => o.OrderId == orderId);
+        }
+
     }
 }
