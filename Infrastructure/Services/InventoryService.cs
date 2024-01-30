a130669100ec45108985e9ae4116734a86eb3d0a
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-01-30 13:41:24 +0100

    + Added Inventory + Order services.

--- Infrastructure/Services/InventoryService.cs
+++ Infrastructure/Services/InventoryService.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Infrastructure.Entities;
+using Infrastructure.Repositories;
 
 namespace Infrastructure.Services
 {
-    internal class InventoryService
+    public class InventoryService
     {
+        private readonly ProductRepository _productRepository;
+
+        public InventoryService(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // //Have to include logic to check if a product already exists before adding product
+        public async Task<ProductEntity> AddProductAsync(ProductEntity product)
+        {
+            return await _productRepository.CreateAsync(product);
+        }
+
+        public async Task<bool> UpdateStockLevelAsync(int ProductId, int quantity)
+        {
+            var product = await _productRepository.GetOneAsync(p => p.ProductId == ProductId);
+            if (product != null)
+            { 
+                product.QuantityInStock = quantity;
+                return await _productRepository.UpdateAsync(product);
+            }
+            return false;
+        }
     }
 }
--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Infrastructure.Entities;
+using Infrastructure.Repositories;
 
 namespace Infrastructure.Services
 {
-    internal class OrderService
+    public class OrderService
     {
+        private readonly OrderRepository _orderRepository;
+
+        public OrderService(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<OrderEntity> CreateOrderAsync(OrderEntity order)
+        {
+            return await _orderRepository.CreateAsync(order);
+        }
+
+        public async Task<bool> UpdateOrderStatusAsync(int OrderId, string NewStatus)
+        {
+            return await _orderRepository.UpdateOrderStatusAsync(OrderId, NewStatus);
+        }
     }
 }
