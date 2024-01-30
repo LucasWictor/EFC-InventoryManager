da69fdbb86042cb270602c62790d063326768267
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-01-30 13:33:38 +0100

    +Added base CustomerService code

--- Infrastructure/Services/CustomerService.cs
+++ Infrastructure/Services/CustomerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Entities;
+using Infrastructure.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Services
 {
-    internal class CustomerService
+    public class CustomerService
     {
+        private readonly CustomerRepository _customerRepository;
+
+        public CustomerService(CustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<CustomerEntity> CreateCustomerAsync(CustomerEntity customer)
+
+        {
+            //might add additional business logic here before creating the customer
+            return await _customerRepository.CreateAsync(customer);
+        }
+
+        public async Task<CustomerEntity> GetCustomerByIdAsync(int customerId)
+        {
+            return await _customerRepository.GetOneAsync(c => c.CustomerId == customerId);
+        }
+
+        public async Task<bool> UpdateCustomerAsync(CustomerEntity customer)
+        {
+            return await _customerRepository.UpdateAsync(customer);
+        }
     }
 }
