d1000b3bf2d21e22970c9dea61b78f0d3c223d53
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-01-30 13:22:41 +0100

    +Added async-await to CustomerRepository.

--- Infrastructure/Repositories/CustomerRepository.cs
+++ Infrastructure/Repositories/CustomerRepository.cs
@@ -12,25 +12,42 @@ namespace Infrastructure.Repositories
     {
         public CustomerRepository(DbContext context ) : base(context)
         { 
-
         }
 
-        //retrive customers by email 
-        public async Task<CustomerEntity> GetCustomersByEmailAsync(string email )
+        //Retrive customers by email 
+        public async Task<CustomerEntity> GetCustomerByEmailAsync(string email )
         {
-            return await GetOneAsync(c => c.Email == email);
+           if (string.IsNullOrWhiteSpace(email))
+            {
+                return null;
+            }
+            return await _context.Set<CustomerEntity>()
+                 .Include(c => c.Orders)
+                 .FirstOrDefaultAsync(c => c.Email == email);
         }
 
         //Retrive customers by name
         public async Task<IEnumerable<CustomerEntity>> GetCustomersByNameAsync(string firstName, string lastName)
         {
-            return await GetAllAsync();
+
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+            {
+                return Enumerable.Empty<CustomerEntity>();
+            }
+
+            return await _context.Set<CustomerEntity>()
+                                 .Where(c => c.FirstName == firstName && c.LastName == lastName)
+                                 .ToListAsync();
         }
+
+
         //Get all orders for specific customer
         public async Task<IEnumerable<OrderEntity>> GetCustomerOrdersAsync(int customerId)
         {
-            var customer = await GetOneAsync(c => c.CustomerId == customerId);
-            return customer?.Orders;
+            var customer = await _context.Set<CustomerEntity>()
+                .Include(c => c.Orders)
+                .FirstOrDefaultAsync(c => c.CustomerId == customerId);
+            return customer?.Orders ?? Enumerable.Empty<OrderEntity>();
         }
 
     }
